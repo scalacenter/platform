f402ce49febba05d69662c88e884e01cb3c3afc1
jvican <jorgevc@fastmail.es>
2016-10-28 16:02:56 +0200

    Fix drone yml file
    Tab characters were unexpectedly written by vim.

--- bin/store_credentials.sh
+++ bin/store_credentials.sh
@@ -3,9 +3,11 @@ set -o nounset                              # Treat unset variables as an error
 
 BINTRAY_USERNAME=$1
 BINTRAY_PASSWORD=$2
-CREDENTIALS_FILE="$HOME/.bintray/.credentials"
+BINTRAY_FOLDER="$HOME/.bintray"
+CREDENTIALS_FILE="$BINTRAY_FOLDER/.credentials"
 
 echo "Creating $CREDENTIALS_FILE"
+test -d $BINTRAY_FOLDER || mkdir $BINTRAY_FOLDER
 cat <<EOF >> $CREDENTIALS_FILE
 realm = Bintray API Realm
 host = api.bintray.com
